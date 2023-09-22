6ce835eef5cbe8eab409457d2098906624b90347
Tarun Kaushik <112891230+kaushiktarun05@users.noreply.github.com>
2023-09-23 00:49:01 +0530

    bg change of all

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: 'https://faldi.xyz/',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
-};
--- src/pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import HelloJson from './json/hello.json';
-
-export default function hello(req, res) {
-  res.status(200).json(HelloJson);
-}
